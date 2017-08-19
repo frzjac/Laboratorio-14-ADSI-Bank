2c6010fd11b5e7c086bdaea5e0fd61150898d84d
Franz Jacanamijoy <fajacanamijoy2@misena.edu.co>
2017-08-19 18:58:53 -0500

    Subimos Proyecto

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var postcss = require('gulp-postcss');
+var cssnested = require('postcss-nested');
+var nestedAncestor = require('postcss-nested-ancestors');
+var cssnext = require('postcss-cssnext');
+var mixins = require('postcss-mixins');
+var cssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
+
+// Servidor base de desarrollo
+gulp.task('server', function() {
+  browserSync.init({
+    server:{
+      baseDir: './dist/'
+    }
+  });
+});
+
+// Tarea para procesar PUG (jade)
+gulp.task('jade', function () {
+  return gulp.src('./src/views/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./dist/'))
+  .pipe(browserSync.stream())
+});
+
+// Tarea para procesar el CSS
+gulp.task('css', function() {
+  var procesos = [
+    cssImport(),
+    mixins(),
+    nestedAncestor,
+    cssnested,    
+    cssnext({ browsers:['> 5%','ie 8']})
+  ];
+
+  return gulp.src('./src/css/styles.css')
+    .pipe(postcss(procesos))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream())
+});
+
+// Tarea para escuchar los cambios en los archivos css y pug
+gulp.task('watch', function() {
+  gulp.watch('./src/css/*.css', ['css']);
+  gulp.watch('./src/views/**/*.pug', ['jade']);
+});
+
+// Lanza todas las tareas
+gulp.task('default', ['watch','server']);
